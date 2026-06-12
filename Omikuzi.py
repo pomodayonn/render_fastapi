a954aa110dcc2d537deb9031158eec7b550de21a
pomodayonn <162393360+pomodayonn@users.noreply.github.com>
2026-06-12 12:24:05 +0900

    Update Omikuzi.py

--- Omikuzi.py
+++ Omikuzi.py
@@ -10,7 +10,7 @@ import requests
 
 print("start client")
 
-url = "https://https://render-fastapi-vjt6.onrender.com/upper"
+url = "https://render-fastapi-vjt6.onrender.com/upper"
 
 msg = input("message: ")
 
