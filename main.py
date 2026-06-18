366ae58cff86554dd230e89893e2a3d3a382a2d9
pomodayonn <162393360+pomodayonn@users.noreply.github.com>
2026-06-18 17:11:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from fastapi.responses import HTMLResponse
-from fastapi import FastAPI
-import random
 from typing import Optional
+
+from fastapi import FastAPI
+
+import random  # randomライブラリを追加
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return HTMLResponse("""
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """)
+    return {"message": "Hello World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
-@app.post("/present")
-async def give_present(present):
-        return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
