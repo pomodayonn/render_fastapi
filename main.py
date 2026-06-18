3d8e9c810eb2ebb772b48a672073ded7eccf4917
pomodayonn <162393360+pomodayonn@users.noreply.github.com>
2026-06-18 15:10:10 +0900

    Fix HTMLResponse return in index function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/index")
 def index():
-    html_content = """
+    return HTMLResponse("""
     <html>
         <head>
             <title>Some HTML in here</title>
@@ -16,5 +16,4 @@ def index():
             <h1>Look ma! HTML!</h1>
         </body>
     </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+    """)
