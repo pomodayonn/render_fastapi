a4036c1d4957b221f0cfa45af61730ecaa7cb056
pomodayonn <162393360+pomodayonn@users.noreply.github.com>
2026-06-18 15:01:10 +0900

    Add HTML response endpoint for index page

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from typing import Optional
-
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
+import random
+from typing import Optional
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
